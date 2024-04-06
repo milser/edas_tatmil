21aacecca61bd81c8c529294f826e0440853e428
milser <35532245+milser@users.noreply.github.com>
2024-04-06 19:38:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,6 @@ setup(
     name='Edas_tatmil',
     version='0.1',
     packages=find_packages(),
-    install_requires=[
-        'paquete-dependencia-1',
-        'paquete-dependencia-2',
-        # Lista de paquetes necesarios para tu proyecto
-    ],
     # Metadatos adicionales del proyecto
     author='Tatiana Cazorla y Rubén Serrano',
     description='Tu EDA mas sencillo',
@@ -16,5 +11,14 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3',
         # Lista de clasificaciones de compatibilidad con Python, SO, etc.
+    ],
+    install_requires=[
+        pandas
+        matplotlib
+        seaborn
+        os
+        math
+        importlib
+        tabulate
     ],
 )
